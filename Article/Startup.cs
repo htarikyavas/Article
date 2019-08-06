5167c1dc93fb8d60f2673a8c02d2ba92a392f2a5
Hasan Tarık YAVAŞ <htarikyavas@gmail.com>
2019-08-06 17:27:07 +0300

    Configure EF Context & Connection

--- Article/Startup.cs
+++ Article/Startup.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Article.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Article
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
+            _config = config;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+              .AddDbContext<ArticleContext>(options =>
+                  options.UseSqlServer(_config["database"], builder => builder.MigrationsAssembly(typeof(ArticleContext).Assembly.FullName))
+              ).AddMvcCore()
+                .AddJsonFormatters();
+
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
