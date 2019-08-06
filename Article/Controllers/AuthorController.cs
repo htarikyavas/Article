fbe01cf4cb3f98b4cd2ceba418e47ca4d0e90b7d
Hasan Tarık YAVAŞ <htarikyavas@gmail.com>
2019-08-06 20:44:17 +0300

    Add AuthorController

---
+++ Article/Controllers/AuthorController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Article.Core.Domain;
+using Article.Services.Authors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Article.WebApi.Controllers
+{
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService _authorService)
+        {
+            this._authorService = _authorService;
+        }
+
+        [HttpGet("author")]
+        public async Task<List<AuthorModel>> All()
+        {
+            return await _authorService.All();
+        }
+
+        [HttpGet("author/{id}")]
+        public async Task<AuthorModel> Single(int id)
+        {
+            return await _authorService.Get(id);
+        }
+
+        [HttpDelete("author/{id}")]
+        public async Task Delete(int id)
+        {
+            await _authorService.Delete(id);
+        }
+
+        [HttpPut("author")]
+        public async Task<AuthorModel> Insert([FromBody] AuthorModel author)
+        {
+            var id = await _authorService.Add(author);
+            return await _authorService.Get(id);
+        }
+
+        [HttpPatch("author/{id}")]
+        public async Task<AuthorModel> Update(int id, [FromBody] AuthorModel author)
+        {
+            await _authorService.Update(id, author);
+            return await _authorService.Get(id);
+        }
+    }
+}
