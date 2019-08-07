941ee5dff8958c833be2740f14f11c4d7bd767d5
Hasan Tarık YAVAŞ <htarikyavas@gmail.com>
2019-08-07 08:30:29 +0300

    Add Initial Data

---
+++ Article.Data/Migrations/20190807052212_migrInitialData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Article.Data.Migrations
+{
+    public partial class migrInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+           table: "Author",
+           columns: new[] { "Name", "Email", "Password", "CreatedAt" },
+           values: new object[] { "Hasan Tarık YAVAŞ", "htarikyavas@gmail.com", "123456", DateTime.Now });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
